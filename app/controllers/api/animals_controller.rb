e14d6482c5e07899af828b32bc5d714adf6be2f6
Erin Weston <westonerin1@gmail.com>
2020-11-10 10:43:38 -0500

    initial commit plus index

---
+++ app/controllers/api/animals_controller.rb
@@ -0,0 +1,7 @@
+class Api::AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+    render 'index.json.jb'
+  end
+  
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Animal.create(name:"Puppy", age: 0, colors:"green and red")
+Animal.create(name:"Kitten", age: 0, colors:"purple and black")
